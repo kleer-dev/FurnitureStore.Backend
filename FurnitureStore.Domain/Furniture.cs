6d69457a3114001bb84e1570eb45f579aa8f8c54
KleerZ <escapeobzory@gmail.com>
2022-07-18 16:27:21 +0300

    add entities

---
+++ FurnitureStore.Domain/Furniture.cs
@@ -0,0 +1,15 @@
+﻿namespace FurnitureStore.Domain;
+
+public class Furniture
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public double Lenght { get; set; }
+    public double Height { get; set; }
+    public string Material { get; set; }
+
+    public FurnitureType FurnitureType { get; set; }
+    public Company Company { get; set; }
+    public List<Order> Orders { get; set; }
+}
---
+++ FurnitureStore.Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace FurnitureStore.Domain;
+
+public class User
+{
+    public decimal Balance { get; set; }
+
+    public List<Order> Orders { get; set; }
+}
