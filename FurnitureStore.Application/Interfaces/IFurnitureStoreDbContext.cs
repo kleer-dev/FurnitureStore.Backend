d51c99b6892b5ccba2893ad827f779323ee26cbd
KleerZ <escapeobzory@gmail.com>
2022-07-18 18:31:02 +0300

    add dbcontext

---
+++ FurnitureStore.Application/Interfaces/IFurnitureStoreDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FurnitureStore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureStore.Application.Interfaces;
+
+public interface IFurnitureStoreDbContext
+{
+    DbSet<User> Users { get; set; }
+    DbSet<Order> Orders { get; set; }
+    DbSet<FurnitureType> FurnitureTypes { get; set; }
+    DbSet<Furniture> Furnitures { get; set; }
+    DbSet<Company> Companies { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken);
+}
