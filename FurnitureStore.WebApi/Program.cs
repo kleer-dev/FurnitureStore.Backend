748fb4f84e265cb4ca067e06f87b2acb089041d1
KleerZ <escapeobzory@gmail.com>
2022-07-18 19:50:57 +0300

    init db from dbcontext

--- FurnitureStore.WebApi/Program.cs
+++ FurnitureStore.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using FurnitureStore.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+
+    try
+    {
+        var context = serviceProvider
+            .GetRequiredService<FurnitureStoreDbContext>();
+
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception)
+    {
+
+        throw;
+    }
+}
+
 
 var app = builder.Build();
 
