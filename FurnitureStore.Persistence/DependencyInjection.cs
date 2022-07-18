0e7cc026720c47171a1f83b15aef50fc2088bb80
KleerZ <escapeobzory@gmail.com>
2022-07-18 19:35:10 +0300

    add mediatr dependency injection

--- FurnitureStore.Persistence/DependencyInjection.cs
+++ FurnitureStore.Persistence/DependencyInjection.cs
@@ -1,5 +1,26 @@
-﻿namespace FurnitureStore.Persistence;
+﻿using FurnitureStore.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-internal class DependencyInjection
+namespace FurnitureStore.Persistence;
+
+public static class DependencyInjection
 {
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+
+        services.AddDbContext<FurnitureStoreDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<IFurnitureStoreDbContext>(provider => 
+            provider.GetService<FurnitureStoreDbContext>());
+
+        return services;
+    }
 }
+    
