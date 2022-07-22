feaf17be60aa508078ac1608201dfe9d4684742e
KleerZ <escapeobzory@gmail.com>
2022-07-22 18:40:21 +0300

    add user controller

---
+++ FurnitureStore.WebApi/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using FurnitureStore.Auth.Login;
+using FurnitureStore.Auth.Registration;
+using FurnitureStore.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureStore.WebApi.Controllers;
+
+[ApiController]
+[Route("api/user")]
+public class UserController : BaseController
+{
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> LoginAsync([FromBody] LoginQuery query)
+    {
+        return await Mediator.Send(query);
+    }
+
+    [HttpPost("registration")]
+    public async Task<ActionResult<UserDto>> Registration([FromBody] RegistrationCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+}
