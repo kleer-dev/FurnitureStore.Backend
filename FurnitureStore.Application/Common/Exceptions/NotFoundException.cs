86d5d980f330f62e424d518b59d6597a9c0870f7
KleerZ <escapeobzory@gmail.com>
2022-07-25 15:16:22 +0300

    add custom exceptions

---
+++ FurnitureStore.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace FurnitureStore.Application.Common.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string name, object key)
+        : base($"Entity '{name}' ({key}) not found.")
+    {
+
+    }
+}
