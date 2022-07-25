3f68ed4f51cba5131d189f3f6c2bbefefe3fbdbd
KleerZ <escapeobzory@gmail.com>
2022-07-25 21:08:27 +0300

    add company creation command

---
+++ FurnitureStore.Application/CommandsQueries/Company/Commands/Create/CreateCompanyCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using FurnitureStore.Application.Interfaces;
+using MediatR;
+
+namespace FurnitureStore.Application.CommandsQueries.Company.Commands.Create;
+
+public class CreateCompanyCommandHandler : IRequestHandler<CreateCompanyCommand, long>
+{
+    private readonly IFurnitureStoreDbContext _dbContext;
+
+    public CreateCompanyCommandHandler(IFurnitureStoreDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<long> Handle(CreateCompanyCommand request,
+        CancellationToken cancellationToken)
+    {
+        var company = new Domain.Company
+        {
+            Name = request.Name
+        };
+
+        await _dbContext.Companies.AddAsync(company, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return company.Id;
+    }
+}
