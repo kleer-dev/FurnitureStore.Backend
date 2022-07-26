c629a09a136feac6c026dec29827ce8c700fd3ce
KleerZ <escapeobzory@gmail.com>
2022-07-26 23:02:07 +0300

    remove unused columns from order entity

--- FurnitureStore.Domain/Order.cs
+++ FurnitureStore.Domain/Order.cs
@@ -3,9 +3,6 @@
 public class Order
 {
     public long Id { get; set; }
-    public int Count { get; set; }
-    public decimal OrderPrice { get; set; }
-
-    public User User { get; set; }  
+    public User User { get; set; }
     public Furniture Furniture { get; set; }
 }
