55490953f39c1a2ae8057d6de49cf35124416dd4
KleerZ <escapeobzory@gmail.com>
2022-07-26 23:26:22 +0300

    add company delete command

---
+++ FurnitureStore.Application/CommandsQueries/Company/Commands/Delete/DeleteCompanyCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using FurnitureStore.Application.Common.Exceptions;
+using FurnitureStore.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureStore.Application.CommandsQueries.Company.Commands.Delete;
+
+public class DeleteCompanyCommandHandler : IRequestHandler<DeleteCompanyCommand, Unit>
+{
+    private readonly IFurnitureStoreDbContext _dbContext;
+
+    public DeleteCompanyCommandHandler(IFurnitureStoreDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Unit> Handle(DeleteCompanyCommand request, 
+        CancellationToken cancellationToken)
+    {
+        var company = await _dbContext.Companies
+            .FirstOrDefaultAsync(c => c.Id == request.Id, cancellationToken);
+
+        if (company == null)
+            throw new NotFoundException(nameof(Domain.Company), request.Id);
+
+        _dbContext.Companies.Remove(company);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
