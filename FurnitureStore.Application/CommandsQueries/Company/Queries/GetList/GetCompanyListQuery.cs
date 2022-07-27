a4b23a6b12f692dc8be18bdf754e744aa25e1b56
KleerZ <escapeobzory@gmail.com>
2022-07-27 17:43:33 +0300

    add get company list query

---
+++ FurnitureStore.Application/CommandsQueries/Company/Queries/GetList/GetCompanyListQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace FurnitureStore.Application.CommandsQueries.Company.Queries.GetList;
+
+public class GetCompanyListQuery : IRequest<GetCompanyListVm>
+{
+
+}
---
+++ FurnitureStore.Application/CommandsQueries/Company/Queries/GetList/GetCompanyListVm.cs
@@ -0,0 +1,6 @@
+﻿namespace FurnitureStore.Application.CommandsQueries.Company.Queries.GetList;
+
+public class GetCompanyListVm
+{
+    public IEnumerable<CompanyDto> Companies { get; set; }
+}
