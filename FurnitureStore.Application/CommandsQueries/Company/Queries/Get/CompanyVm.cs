ef0e84c48b6cc3df7171893115b1e3e5acd61b01
KleerZ <escapeobzory@gmail.com>
2022-07-27 19:07:09 +0300

    add get company query

---
+++ FurnitureStore.Application/CommandsQueries/Company/Queries/Get/CompanyVm.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FurnitureStore.Application.Common.Mappings;
+using FurnitureStore.Domain;
+
+namespace FurnitureStore.Application.CommandsQueries.Company.Queries.Get;
+
+public class CompanyVm : IMapWith<Domain.Company>
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public List<Furniture> Furnitures { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Domain.Company, CompanyVm>()
+            .ForMember(c => c.Id,
+                o => o.MapFrom(c => c.Id))
+            .ForMember(c => c.Name,
+                o => o.MapFrom(c => c.Name))
+            .ForMember(c => c.Furnitures,
+                o => o.MapFrom(c => c.Furnitures));
+    }
+}
