0d61c49a4d7c042d908fa136860f71297607ed96
KleerZ <escapeobzory@gmail.com>
2022-07-28 21:55:15 +0300

    add create furniture type method for furniture type controller

---
+++ FurnitureStore.Application/CommandsQueries/FurnitureType/Commands/Create/CreateFurnitureCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace FurnitureStore.Application.CommandsQueries.FurnitureType.Commands.Create;
+
+public class CreateFurnitureCommandValidator : AbstractValidator<CreateFurnitureTypeCommand>
+{
+    public CreateFurnitureCommandValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .MaximumLength(150);
+    }
+}
--- FurnitureStore.WebApi/Controllers/FurnitureTypeController.cs
+++ FurnitureStore.WebApi/Controllers/FurnitureTypeController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using FurnitureStore.Application.CommandsQueries.FurnitureType.Commands.Create;
+using FurnitureStore.WebApi.Dto.FurnitureType;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FurnitureStore.WebApi.Controllers;
 
 [ApiController]
-[Route("api/furniture-type")]
+[Route("api/furniture-types")]
 public class FurnitureTypeController : BaseController
 {
+    private readonly IMapper _mapper;
 
+    public FurnitureTypeController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<long>> Create([FromBody] CreateFurnitureTypeDto dto)
+    {
+        var command = _mapper.Map<CreateFurnitureTypeCommand>(dto);
+        var furnitureTypeId = await Mediator.Send(command);
+
+        return Created("api/furniture-types", furnitureTypeId);
+    }
 }
