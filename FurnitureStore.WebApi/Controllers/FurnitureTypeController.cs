fc887e87d497c0d2393fac90a36e204437e5c48c
KleerZ <escapeobzory@gmail.com>
2022-07-28 16:08:14 +0300

    add furniture type controller

---
+++ FurnitureStore.WebApi/Controllers/FurnitureTypeController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureStore.WebApi.Controllers;
+
+[ApiController]
+[Route("api/furniture-type")]
+public class FurnitureTypeController : BaseController
+{
+
+}
