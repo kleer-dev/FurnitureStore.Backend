de7545f044a73127ae926a77db88ecf2c90701ba
KleerZ <escapeobzory@gmail.com>
2022-07-29 15:08:19 +0300

    add furniture controller

---
+++ FurnitureStore.WebApi/Controllers/FurnitureController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureStore.WebApi.Controllers;
+
+[ApiController]
+[Route("api/furnitures")]
+public class FurnitureController : BaseController
+{
+
+}
