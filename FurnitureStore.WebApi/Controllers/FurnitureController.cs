b6bc2822aa091254468c472468eed1a83a7fef0e
KleerZ <escapeobzory@gmail.com>
2022-07-29 16:18:48 +0300

    add delete method for furniture controller

--- FurnitureStore.WebApi/Controllers/FurnitureController.cs
+++ FurnitureStore.WebApi/Controllers/FurnitureController.cs
@@ -1,4 +1,7 @@
-﻿using FurnitureStore.Application.CommandsQueries.Furniture.Commands.Create;
+﻿using System;
+using System.Threading.Tasks;
+using FurnitureStore.Application.CommandsQueries.Furniture.Commands.Create;
+using FurnitureStore.Application.CommandsQueries.Furniture.Commands.Delete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FurnitureStore.WebApi.Controllers;
@@ -13,5 +16,14 @@ public class FurnitureController : BaseController
         var furnitureId = await Mediator.Send(furniture);
 
         return Created("api/furnitures", furnitureId);
+    }
+
+    [HttpDelete("{id:long}")]
+    public async Task<ActionResult> Delete(long id)
+    {
+        var command = new DeleteFurnitureCommand() { Id = id };
+        await Mediator.Send(command);
+
+        return NoContent();
     }
 }
