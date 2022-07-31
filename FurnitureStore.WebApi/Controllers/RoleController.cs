f30fb90fcb40d2c2c2a134a92f09dfd836c2b06a
KleerZ <escapeobzory@gmail.com>
2022-07-31 15:52:51 +0300

    add create method for role controller

--- FurnitureStore.WebApi/Controllers/RoleController.cs
+++ FurnitureStore.WebApi/Controllers/RoleController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FurnitureStore.Application.CommandsQueries.Role.Commands.Create;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FurnitureStore.WebApi.Controllers;
 
 [Route("api/roles")]
 public class RoleController : BaseController
 {
-    
+    [HttpPost("add")]
+    public async Task<ActionResult<long>> Create([FromBody] CreateRoleCommand role)
+    {
+        var roleId = await Mediator.Send(role);
+
+        return Created("api/roles", roleId);
+    }
 }
